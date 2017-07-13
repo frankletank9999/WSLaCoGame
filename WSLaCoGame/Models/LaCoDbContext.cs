6c80d7b64fece231c948b61e275ad7bf45019cc1
home9999 <fcote27@gmail.com>
2017-07-13 11:39:38 -0400

    new

---
+++ WSLaCoGame/Models/LaCoDbContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace WSLaCoGame.Models
+{
+    public class LaCoDbContext:DbContext
+    {
+        public LaCoDbContext(DbContextOptions<LaCoDbContext> options)
+            : base(options)
+        {
+
+        }
+        public DbSet<TodoItem> TodoItems { get; set; }
+
+    }
+}
